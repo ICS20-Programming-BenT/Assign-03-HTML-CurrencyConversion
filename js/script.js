5b4755d2cf07cdcafc4f804df7ebdb36b851f059
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-04-14 00:36:59 +0000

    Updated index.html and script.js

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@
 
 // This function converts CAD to GBP and EUR.
 function convertCanadianDollars () {
-  // Get the user input
+  // Get the user input in Canadian Dollars.
   let amountCanadianDollars = parseFloat(document.getElementById("amount-canadian-dollars").value);
 
-  // Perform mathematical equations
+  // Perform mathematical equations to convert to pounds and euros with conversion rates
   let amountBritishPounds = amountCanadianDollars * 0.5967
   let amountEuros = amountCanadianDollars * 0.6791
 
-  // Display calculations back to user
+  // Display coverted amounts in GBP and EUR back to user
   document.getElementById("calculations").innerHTML = "The value of your money in British pounds sterling is £" + amountBritishPounds.toFixed(2) + ", and the the value in Euros is €" + amountEuros.toFixed(2) + "."
 }
